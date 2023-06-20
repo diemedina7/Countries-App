ca19c44585eca361afbf5117e6d75b41910e4c7d
diemedina7 <diegocab456@gmail.com>
2023-06-20 18:58:42 -0300

    Countries App Angular 16

---
+++ src/app/app-routing.module.ts
@@ -0,0 +1,40 @@
+import { NgModule } from '@angular/core'
+import { Routes, RouterModule } from '@angular/router'
+
+import { AboutPageComponent } from './shared/pages/about-page/about-page.component'
+import { ContactPageComponent } from './shared/pages/contact-page/contact-page.component'
+import { HomePageComponent } from './shared/pages/home-page/home-page.component'
+
+const routes: Routes = [
+  /*{
+    path: '',
+    component: HomePageComponent
+  },*/
+  {
+    path: 'about',
+    component: AboutPageComponent
+  },
+  {
+    path: 'contact',
+    component: ContactPageComponent
+  },
+  {
+    path: 'countries',
+    loadChildren: () => import('./countries/countries.module').then(module => module.CountriesModule)
+    // loadChildren : promise (lazy load) -> import('/*path del modulo a cargar*/').then(m => m.class) //m.class = clase del moudulo exportado
+  },
+  {
+    path: '**', // otherwise...
+    redirectTo: 'countries'
+  }
+]
+
+@NgModule({
+  imports: [
+    RouterModule.forRoot(routes)
+  ],
+  exports: [
+    RouterModule
+  ]
+})
+export class AppRoutingModule {}
---
+++ src/app/countries/services/countries.service.ts
@@ -0,0 +1,80 @@
+import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Observable, catchError, map, of, tap } from 'rxjs';
+
+import { Country } from '../interfaces/country.interface';
+import { CacheStore } from '../interfaces/cache-store.interface';
+import { Region } from '../interfaces/region.type';
+
+@Injectable({providedIn: 'root'})
+export class CountriesService {
+
+  private apiUrl: string = 'https://restcountries.com/v3.1';
+
+  public cacheStore: CacheStore = {
+    byCapital: { term: '', countries: [] },
+    byCountry: { term: '', countries: [] },
+    byRegion: { countries: [] } //Region no tiene valor al iniciarse porque todavia no se hizo la busqueda
+  }
+
+  constructor(private http: HttpClient) {
+    this.loadFromLocalStorage();
+  }
+
+  private saveToLocalStorage() {
+    localStorage.setItem('cacheStore', JSON.stringify(this.cacheStore));
+  }
+
+  private loadFromLocalStorage() {
+    if(!localStorage.getItem('cacheStore'))
+      return
+
+    this.cacheStore = JSON.parse(localStorage.getItem('cacheStore')!);
+  }
+
+  private getCountriesRequest(url: string) : Observable<Country[]> {
+    // pipe para trabajar con el response de la peticion http
+    return this.http.get<Country[]>(url)
+      .pipe(
+        catchError(() => of([])),
+        //delay(2000) //-> para detener el response de la peticion x milisegundos
+      );
+  }
+
+  public searchCountryByAlphaCode(code: string): Observable<Country | null> {
+    const url = `${this.apiUrl}/alpha/${code}`;
+
+    return this.http.get<Country[]>(url)
+      .pipe(
+        map( countries => countries.length > 0 ? countries[0] : null),
+        catchError(() => of(null))
+      );
+  }
+
+  public searchCapital(term: string): Observable<Country[]> {
+    const url = `${this.apiUrl}/capital/${term}`;
+    return this.getCountriesRequest(url)
+      .pipe(
+        tap(countries => this.cacheStore.byCapital = {term, countries}),
+        tap(() => this.saveToLocalStorage())
+      );
+  }
+
+  public searchCountry(term: string): Observable<Country[]> {
+    const url = `${this.apiUrl}/name/${term}`;
+    return this.getCountriesRequest(url)
+      .pipe(
+        tap(countries => this.cacheStore.byCountry = {term, countries}),
+        tap(() => this.saveToLocalStorage())
+      );
+  }
+
+  public searchRegion(region: Region): Observable<Country[]> {
+    const url = `${this.apiUrl}/region/${region}`;
+    return this.getCountriesRequest(url)
+      .pipe(
+        tap(countries => this.cacheStore.byRegion = {region, countries}),
+        tap(() => this.saveToLocalStorage())
+      );
+  }
+}
